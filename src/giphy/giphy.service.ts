b63cfed7b043a247e2bfac71a12ab31f3ea5667b
GrimAveira <st.tryed@gmail.com>
2023-09-28 18:25:50 +0400

    Add DTO giphy and exchange, fix templates

--- src/giphy/giphy.service.ts
+++ src/giphy/giphy.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { ResponseGiphyAPI, ResponseGiphyDTO } from './dto/response-giphy.dto';
 
 @Injectable()
 export class GiphyService {
-	async getGiphyByTag(key_word: string): Promise<any> {
+	async getGiphyByTag(key_word: string): Promise<ResponseGiphyDTO[] | Error> {
 		try {
-			const res = await axios.get(
+			const res = await axios.get<ResponseGiphyAPI>(
 				`${process.env.GIPHY_URL}/v1/gifs/search?api_key=${process.env.GIPHY_APP_KEY}
 			&limit=50&q=${key_word}`,
 			);
-
 			return res.data.data.map((item) => {
-				return { title: item.title, id: item.id, embed_url: item.embed_url };
+				return { title: item.title, embed_url: item.embed_url };
 			});
 		} catch (e) {
-			return `ERROR: ${e}`;
+			return e;
 		}
 	}
 }
