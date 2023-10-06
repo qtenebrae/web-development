590e572fcde092d63cd20d858d6418202362e26a
GrimAveira <st.tryed@gmail.com>
2023-10-06 15:03:19 +0400

    Clean React Change favicon icon, project.json favicon and main component Delete React spec

---
+++ apps/frontend/src/app/CurrencyTable.tsx
@@ -0,0 +1,5 @@
+export function CurrencyTable() {
+  return <div></div>;
+}
+
+export default CurrencyTable;
--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
+import CurrencyTable from './CurrencyTable';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="frontend" />
+      <CurrencyTable />
     </div>
   );
 }
